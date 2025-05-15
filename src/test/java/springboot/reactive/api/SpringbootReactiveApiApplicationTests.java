5d04dbd3d5dcc5d1073107a9079e58ef449884fe
Labib BENALI <labib9774@gmail.com>
2025-05-15 20:05:12 +0200

    first commit

---
+++ src/test/java/springboot/reactive/api/SpringbootReactiveApiApplicationTests.java
@@ -0,0 +1,13 @@
+package springboot.reactive.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootReactiveApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
